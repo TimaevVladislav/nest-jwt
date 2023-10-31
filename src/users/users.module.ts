483f2b51ea151b71f94cad88853b5b6cc01d16d1
timaevV <timaevvladislav@gmail.com>
2023-11-01 03:46:56 +0600

    feat:relation role with user

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,12 +3,13 @@ import { UsersService } from "./users.service"
 import { UsersController } from "./users.controller"
 import {SequelizeModule} from "@nestjs/sequelize"
 import {User} from "./users.model"
+import {Role} from "../roles/roles.model"
 
 @Module({
   providers: [UsersService],
   controllers: [UsersController],
   imports: [
-      SequelizeModule.forFeature([User])
+      SequelizeModule.forFeature([User, Role])
   ]
 })
 
