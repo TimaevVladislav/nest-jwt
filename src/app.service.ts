107907f4719171d2ae2d5351a65077ca88c4efbc
timaevV <timaevvladislav@gmail.com>
2023-10-31 23:59:20 +0600

    feat:database module

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@nestjs/common"
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!'
+  getUsers(): Array<{id: number, name: string}> {
+    return [{id: 1, name: "Vlad"}, {id: 2, name: "Elena"}]
   }
 }
