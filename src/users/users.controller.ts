287d85fec8ed3bd3239a139e9fc1b6cfac875008
timaevV <timaevvladislav@gmail.com>
2023-11-01 03:00:08 +0600

    feat:user module register with dto

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from "@nestjs/common"
+import {Controller, Post, Get, Body} from "@nestjs/common"
+import {UsersService} from "./users.service"
+import {RegisterDto} from "./dto/register.dto"
+import {ApiOperation} from "@nestjs/swagger";
 
 @Controller("users")
-export class UsersController {}
+
+export class UsersController {
+
+    constructor(private usersService: UsersService) {}
+
+    @Post()
+    register(@Body() userDto: RegisterDto) {
+      return this.usersService.register(userDto)
+    }
+
+    @Get()
+    getAllUsers() {
+        return this.usersService.getAllUsers()
+    }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from "@nestjs/common"
+import {InjectModel} from "@nestjs/sequelize"
+import {User} from "./users.model"
+import {RegisterDto} from "./dto/register.dto"
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(@InjectModel(User) private userRepository: typeof User) {}
+
+    async register(dto: RegisterDto) {
+       const user = await this.userRepository.create(dto)
+       return user
+    }
+
+    async getAllUsers(): Promise<User[]> {
+        const users = await this.userRepository.findAll()
+        return users
+    }
+}
