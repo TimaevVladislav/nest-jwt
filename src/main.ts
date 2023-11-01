65ce5f7e8f13ab45c229d3dc049ad23a8f9f7843
timaevV <timaevvladislav@gmail.com>
2023-11-02 04:44:05 +0600

    fix:prosses port

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import * as process from "process"
 import { NestFactory } from "@nestjs/core"
 import { AppModule } from "./app.module"
 import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger"
