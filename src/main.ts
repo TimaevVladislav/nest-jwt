a82e165ce0b9af05e2c07cfd72ee3bd5fa49f17d
timaevV <timaevvladislav@gmail.com>
2023-11-02 04:02:56 +0600

    feat:global prefix

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger"
 async function start() {
   const PORT = process.env.PORT || 5000
   const app = await NestFactory.create(AppModule)
+  app.setGlobalPrefix("api")
 
   const config = new DocumentBuilder()
         .setTitle("Nest travel API")
