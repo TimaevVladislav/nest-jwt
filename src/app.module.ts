90bc9b56a4bad9a415f371456300e7d37a7582c1
timaevV <timaevvladislav@gmail.com>
2023-11-04 17:23:40 +0600

    feat: static files from serve

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from "@nestjs/common"
 
 import * as process from "process"
+import * as path from "path"
+
+import {ServeStaticModule} from "@nestjs/serve-static"
 import {SequelizeModule} from "@nestjs/sequelize"
 import {ConfigModule} from "@nestjs/config"
 
@@ -24,6 +27,7 @@ import {UserRoles} from "../relations/user.roles.model"
   providers: [],
   imports: [
     ConfigModule.forRoot({envFilePath: `.${process.env.NODE_ENV}.env`}),
+    ServeStaticModule.forRoot({rootPath: path.resolve(__dirname, "static")}),
     SequelizeModule.forRoot({
       dialect: "postgres",
       host: process.env.DATABASE_HOST,
